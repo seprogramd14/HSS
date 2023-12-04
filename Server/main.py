48e609b7394cecfa4949571d08e20cb6bba6ce11
Sepro <shtmdwns0@gmail.com>
2023-12-04 20:18:35 +0900

    main Cors 설정

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import user, video
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(video.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
