d4016e47c877222e475c8b77804dd75e3aea5909
Sepro <shtmdwns0@gmail.com>
2023-12-10 16:59:10 +0900

    로그인, 회원가입 페이지

--- HSS_App/lib/main.dart
+++ HSS_App/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HSS',
-      home: MyPage(),
+      home: SignUp(),
       theme: ThemeData(
         splashColor: Colors.transparent,
         highlightColor: Colors.transparent,
@@ -23,7 +21,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
+// 네이게이터 바
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
 
@@ -175,6 +173,207 @@ class MyInfoPage extends StatelessWidget {
                 onPressed: () {},
                 child: Text('회원 탈퇴', style: TextStyle(color: Color(0xffff0000), fontSize: 24),)
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 로그인 페이지
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final OutlineInputBorder border = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      borderSide: BorderSide(
+        width: 3,
+        color: Color(0xffD3EAFF),
+      ),
+    );
+
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      primary: Color(0xffD3EAFF),
+      minimumSize: Size(100, 50),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff99CFFF),
+        title: Text('로그인', style: TextStyle(color: Colors.white),),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 57),
+            Image.asset('Image/Logo.jpg'),
+            Container(
+              width: 234,
+              height: 50,
+              margin: EdgeInsets.fromLTRB(0, 32, 0, 0),
+              child: TextField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  labelText: '유저 이름',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 234,
+              height: 50,
+              margin: EdgeInsets.fromLTRB(0, 19, 0, 0),
+              child: TextField(
+                obscureText: true,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  labelText: '비밀번호',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 11, 0, 0),
+              child: Text(
+                '회원가입을 아직 안하셨나요?',
+                style: TextStyle(
+                  color: Color(0xff0075ff),
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 45,
+              margin: EdgeInsets.fromLTRB(0, 49, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    style: style,
+                    onPressed: () {},
+                    child: Text('로그인', style: TextStyle( color: Colors.black, fontSize: 15),)
+                  ),
+                  ElevatedButton(
+                    style: style,
+                    onPressed: () {},
+                    child: Text('취소', style: TextStyle( color: Colors.black, fontSize: 15),),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 회원가입 페이지
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final OutlineInputBorder border = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      borderSide: BorderSide(
+        width: 3,
+        color: Color(0xffD3EAFF),
+      ),
+    );
+
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      primary: Color(0xffD3EAFF),
+      minimumSize: Size(100, 50),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회원가입', style: TextStyle(color: Colors.white),),
+        backgroundColor: Color(0xff99CFFF),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 57),
+            Image.asset('Image/Logo.jpg'),
+            Container(
+              width: 234,
+              height: 50,
+              margin: EdgeInsets.fromLTRB(0, 32, 0, 0),
+              child: TextField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  labelText: '유저 이름',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 234,
+              height: 50,
+              margin: EdgeInsets.fromLTRB(0, 19, 0, 0),
+              child: TextField(
+                obscureText: true,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  labelText: '비밀번호',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 234,
+              height: 50,
+              margin: EdgeInsets.fromLTRB(0, 19, 0, 0),
+              child: TextField(
+                obscureText: true,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  labelText: '비밀번호 확인',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 45,
+              margin: EdgeInsets.fromLTRB(0, 35, 0, 0),
+              child: ElevatedButton(
+                  style: style,
+                  onPressed: () {},
+                  child: Text('회원가입', style: TextStyle( color: Colors.black, fontSize: 18),)
+              ),
+            )
           ],
         ),
       ),
